b14787ee5eb860b7b29b824b5ffede3ee1953cad
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-03-19 12:34:16 +0100

    add ternary for error in loader-fnc

--- frontend/src/pages/EventsPage.js
+++ frontend/src/pages/EventsPage.js
@@ -4,7 +4,12 @@ import EventsList from "../components/EventsList";
 
 function EventsPage() {
   const data = useLoaderData();
+
+  if (data.isError) {
+    return <p>{data.message}</p>;
+  }
   const events = data.events;
+
   return <EventsList events={events} />;
 }
 
@@ -14,7 +19,7 @@ export async function loader() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    // ...
+    return { isError: true, message: "Could not fetch events." };
   } else {
     return response;
   }
