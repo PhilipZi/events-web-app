bd7a82d0b37d34c1ee1c6de11c6ff2e1c8723a66
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-03-19 12:12:28 +0100

    add useNavigation as comment add useTimout for backend fetch req

--- frontend/src/pages/RootLayout.js
+++ frontend/src/pages/RootLayout.js
@@ -2,9 +2,12 @@ import { Outlet } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation";
 
 function RootLayout() {
+  // const navigation = useNavigation();
+
   return (
     <>
       <MainNavigation />
+      {/* {navigation.state === "loading" && <p>Loading...</p>} */}
       <main>
         <Outlet />
       </main>
