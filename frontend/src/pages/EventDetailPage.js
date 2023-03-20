b90ce96ec0927380d726a2bd126c1ad768b0a9e0
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-03-20 13:06:36 +0100

    add useRouteLoaderData change path for children loader

--- frontend/src/pages/EventDetailPage.js
+++ frontend/src/pages/EventDetailPage.js
@@ -1,11 +1,13 @@
-import { useLoaderData, json } from "react-router-dom";
+import { useRouteLoaderData, json } from "react-router-dom";
+
 import EventItem from "../components/EventItem";
 
 function EventDetailPage() {
-  const data = useLoaderData();
+  const data = useRouteLoaderData("event-detail");
 
   return <EventItem event={data.event} />;
 }
+
 export default EventDetailPage;
 
 export async function loader({ request, params }) {
@@ -14,9 +16,11 @@ export async function loader({ request, params }) {
   const response = await fetch("http://localhost:8080/events/" + id);
 
   if (!response.ok) {
-    return json(
+    throw json(
       { message: "Could not fetch details for selected event." },
-      { status: 500 }
+      {
+        status: 500,
+      }
     );
   } else {
     return response;
