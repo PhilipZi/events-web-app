280a3ca0d6d93a75185efc3843b25e744566806c
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-03-20 13:15:04 +0100

    change Form element in EventForm component

--- frontend/src/pages/NewEventPage.js
+++ frontend/src/pages/NewEventPage.js
@@ -4,3 +4,7 @@ function NewEventPage() {
   return <EventForm />;
 }
 export default NewEventPage;
+
+export async function action(){
+  fetch("http://localhost:8080/events", {method: "POST", body: })
+}
