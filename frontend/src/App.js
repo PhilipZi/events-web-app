b19ef8e61b175852c5ee46f820d2221dc144b6d0
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-03-22 19:14:02 +0100

    fix git hub issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ import NewsletterPage, { action as newsletterAction } from "./pages/Newsletter";
 import AuthenticationPage, {
   action as authAction,
 } from "./pages/Authentication";
-import { action as logoutAction } from "./pages/Logout";
+import { action as actionLogout } from "./pages/Logout";
 import { checkAuthLoader, tokenLoader } from "./util/auth";
 
 const router = createBrowserRouter([
@@ -75,7 +75,7 @@ const router = createBrowserRouter([
       },
       {
         path: "logout",
-        action: logoutAction,
+        action: actionLogout,
       },
     ],
   },
